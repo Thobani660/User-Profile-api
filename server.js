b798428e34dcc719f222e5a59cf57ecf453c2532
Thobani660 <zondit660@gmail.com>
2024-11-01 14:16:01 +0200

    added:app use to merge express and json

--- server.js
+++ server.js
@@ -1,2 +1,8 @@
 const express = require("express")
 const userRoute = require("./routes/userRoutes")
+const errorHandler = require("./middleware/errorHandler")
+const app = express(App)
+const port = 5050
+
+app.use(express.json())
+use("./routes/userRoutes.js")
